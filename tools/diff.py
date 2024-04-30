96d3c93f5d2b884d1e7fef09d1d7ee00c02992df
lxzqaq <2982029868@qq.com>
2024-04-30 23:23:30 +0800

    UPDATE ACTION

--- tools/diff.py
+++ tools/diff.py
@@ -3,9 +3,9 @@ from jsondiff import diff
 
 def compare_json_files(file1_path, file2_path):
     # 读取JSON文件
-    with open(file1_path, 'r') as file:
+    with open(file1_path, 'r', encoding='utf-8') as file:
         data1 = json.load(file)
-    with open(file2_path, 'r') as file:
+    with open(file2_path, 'r', encoding='utf-8') as file:
         data2 = json.load(file)
     
     # 提取ClientDataVersion字段值
@@ -19,13 +19,13 @@ def compare_json_files(file1_path, file2_path):
             with open('is_run.txt', 'w') as file:
                 file.write('UPDATE')
         else:
-            print("ClientDataVersion in ./json/1.6.0/CNRELAndroid1.6.0.json is greater or equal.")
+            print("NO UPDATE")
     else:
         print("Error: ClientDataVersion not found in one or both files.")
 
 # 两个JSON文件的路径
 file1_path = './4.6.0/CNRELAndroid4.6.0.json'
-file2_path = './json/1.6.0/CNRELAndroid1.6.0.json'
+file2_path = './json/4.6.0/CNRELAndroid4.6.0.json'
 
 # 调用比较函数
 compare_json_files(file1_path, file2_path)
